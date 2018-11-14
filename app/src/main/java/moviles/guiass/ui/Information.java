611e439945d8b64bdc476604db79f204221260a8
Angel Arciniega <MiguelAngel.Arciniega@skyworksinc.com>
2018-11-13 20:55:41 -0800

    New Activiy Login & Explaining in spanish

--- app/src/main/java/moviles/guiass/ui/Information.java
+++ app/src/main/java/moviles/guiass/ui/Information.java
@@ -16,20 +16,53 @@ import moviles.guiass.R;
 
 public class Information extends AppCompatActivity {
 
+
+    // se hacen binding por medio de butterknife a los objetos de la vista XML en este caso a los TEXTVIEW'S
+    // para ello se les asignan mediante los ID's txt_infoTitle, txt_infoDesc que ya se debieron haber asignado en el XML
     @BindViews({R.id.txt_infoTitle, R.id.txt_infoDesc})
     List<TextView> titles;
 
+
+    // El metodo onCreate se inicializa para crear todos los objetos o darles algun valor
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // se manda a llamar la funcioin super que en definicion es como un constructor que guarda la instancia del estado actual
         super.onCreate(savedInstanceState);
+
+        // aqui se asigna la vista al Activity en este caso estamos en el activity Information
+        // y se  le esta asignando la vista del siguiente repositorio
+        // R.layout.activity_information
+        // que en definicion por carpetas: res -> layout -> activity_information.xml
         setContentView(R.layout.activity_information);
+
+        //aqui damos por hecho que haremos binding a los objetos ya declarados anteriormente para este Activity
         ButterKnife.bind(this);
+
+        // con el getIntent().getExtras() accedemos a todos los datos que recive este activity de otro en este caso
+        // el activity main envia un parametro llamado InfoOption al activity Information y lo guardamos en una variable
+        // int option
         Bundle bundle = getIntent().getExtras();
         int option = bundle.getInt("InfoOption");
+
+        //con el valor obtenido ya de la actividad anterior realizamos un cast por medio de la clase
+        // MenuOptionsEnum accediendo a su metodo valueOf y enviandole como parametro option
         MenuOptionsEnum selected = MenuOptionsEnum.valueOf(option);
+
+
+        // se asignan los datos por default de la vista de Information
         String title = "Informacion";
         String description = "N/A";
-        System.out.println(option);
+
+
+        //Se realiza una comparacion en base al resultado que nos arroje la variable select
+        // Este resultado se realiza mediante MenuOptionsEnum que es una clase de tipo ENUM(enumerable)
+        // esta clase enumerable solo enumera N numero de datos para poder acceder a ellos de manera mas facil
+        // en ves de comparar los datos como:
+        // si es igual a 1, 2, 3, o 4
+        // mas facil compararlos de la siguiente manera:
+        // si es RequisitosPrevios, DatosDelServicioSocial, PrimerosPasos.... etc.
+        // Si deceas saber mas como funciona el enumerable revisar la clase localizada en:
+        // moviles -> guiass -> Enum -> MenuOptionsEnum.java
         switch (selected) {
             case RequisitosPrevios:
                 description = "Requisitos Previos";
@@ -50,6 +83,9 @@ public class Information extends AppCompatActivity {
                 description = "Unknown";
 
         }
+        // como anteriormente teniamos una lista de TEXTVIEWS solo accedemos al text view con su respectiva posicion
+        // posicion 0 y 1
+        // y se ingresan los textos de descripcion y titulo.
         titles.get(0).setText(title);
         titles.get(1).setText(description);
     }
--- app/src/main/java/moviles/guiass/ui/MainActivity.java
+++ app/src/main/java/moviles/guiass/ui/MainActivity.java
@@ -16,26 +16,61 @@ import static moviles.guiass.Enum.MenuOptionsEnum.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    // se hacen binding por medio de butterknife a los objetos de la vista XML en este caso a los TEXTVIEW'S
+    // para ello se les asignan mediante los ID's txt_infoTitle, txt_infoDesc que ya se debieron haber asignado en el XML
     @BindViews({R.id.btn_req, R.id.btn_dh, R.id.btn_dss, R.id.btn_pp, R.id.btn_ps})
     List<Button> btnsInfo;
 
+    // El metodo onCreate se inicializa para crear todos los objetos o darles algun valor
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // se manda a llamar la funcioin super que en definicion es como un constructor que guarda la instancia del estado actual
         super.onCreate(savedInstanceState);
+
+        // se asigna la vista al Activity en este caso estamos en el activity Information
+        // y se  le esta asignando la vista del siguiente repositorio
+        // R.layout.activity_information
+        // que en definicion por carpetas: res -> layout -> activity_main.xml
         setContentView(R.layout.activity_main);
+
+        //damos por hecho que haremos binding a los objetos ya declarados anteriormente para este Activity
         ButterKnife.bind(this);
+
+        // realizamos un for para asignarle a cada boton un onClickListener
+        // ya asinados por la lista que se realizo antes en butterknife
+        // y etse nos servira para escuchar el clicka de cada boton
+        // se le envia como parametro una vista en etse caso usamos la palabra reservada this
+        // que significa esto o esta clase en la que te encuentras actualmente ya que esta clase es
+        //una vista se puede enviar sin problema alguna
         for (Button btn : btnsInfo) {
             btn.setOnClickListener(this);
         }
     }
 
-
+    // aqui se encuentra el listener que se les asigno a los botones anteriormente
     @Override
     public void onClick(View v) {
+
+        // Con la clase o metodo Intent podremos mandar a llamar a otro activity
+        // para ello se crea un intent en el cual recive dos parametros
+        // 1: el contexto del paquete de donde se enviara en este caso es desde la clase MainActivity.this el cual es la clase del activity
+        // 2: la clase que del activity que quieres mandar a llamar en etse claso Information.class para acceder a su clase
         Intent intent = new Intent(MainActivity.this, Information.class);
+
+        // creamos una variable bundle para almacenar las variables que le queremos enviar al activity a invocar
         Bundle bundle = new Bundle();
+
+        // ya que estamos dentro del metodo onclick realizamos una comparacion para ver desde
+        // que boton estamos presionando
         switch (v.getId()) {
+
+            //en cualquiera de los casos agrega al buldle un parametro con su respectivo valor
+            // hay distintos modos de enviar parametros en este caso sera una variable entera por ello
+            // se usa el metodo putInt... en cado caso de ser un string seria putString e igual para
+            // cualquier otro tipo de dato
+            // para ello se hace lo siguiente bundle.putInt("NOMBRE_DEL_PARAMETRO_A_ENVIAR", VALOR_DEL_PARAMETRO);
+
             case R.id.btn_req:
                 bundle.putInt("InfoOption", RequisitosPrevios.getValue());
 
@@ -56,7 +91,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 break;
         }
+        // Con nuestra variable intent accedemos a su metodo putExtras para asignarle eseos parametros
+        //cpon variables que ya se les asigno anteriormente
         intent.putExtras(bundle);
+
+        // se inicializa el nuevo activity
         startActivity(intent);
     }
 
