db63c61ef261c0015c8813777a465be73a60294f
AGUSTINvidal <42757199+AGUSTINvidal@users.noreply.github.com>
2018-11-21 17:48:22 -0800

    Modification in login
    Add InterfaceSecion, TeacherInterface and layout
    Modification Login

---
+++ app/src/main/java/moviles/guiass/ui/interfaceActivity/GraphicInterfaceSecion.java
@@ -0,0 +1,14 @@
+package moviles.guiass.ui.interfaceActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import moviles.guiass.R;
+
+public class GraphicInterfaceSecion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic_interface_secion);
+    }
+}
