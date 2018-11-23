8afe4b8709bab1648d9ea8fc85a082c7494c56a8
Angel Arciniega <mangel.arciniega@gmail.com>
2018-11-22 17:53:14 -0800

    Add user Interface

---
+++ app/src/main/java/moviles/guiass/ui/interfaceActivity/InterfaceUser.java
@@ -0,0 +1,14 @@
+package moviles.guiass.ui.interfaceActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import moviles.guiass.R;
+
+public class InterfaceUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface_user);
+    }
+}
