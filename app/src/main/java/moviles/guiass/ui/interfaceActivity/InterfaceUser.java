e4ed5021382485467cfc3e707e850bb4f4cfc42b
AGUSTINvidal <42757199+AGUSTINvidal@users.noreply.github.com>
2018-11-23 11:21:59 -0800

    Modify Activitys
    Modify Activity Information, plandetrabajo, prosesopasopaso, reportes and toobar.

--- app/src/main/java/moviles/guiass/ui/interfaceActivity/InterfaceUser.java
+++ app/src/main/java/moviles/guiass/ui/interfaceActivity/InterfaceUser.java
@@ -2,7 +2,8 @@ package moviles.guiass.ui.interfaceActivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import moviles.guiass.R;
+
+import moviles.guiass.ui.R;
 
 public class InterfaceUser extends AppCompatActivity {
 
--- app/src/main/java/moviles/guiass/ui/lateralmenu/MenuSliteActivity.java
+++ app/src/main/java/moviles/guiass/ui/lateralmenu/MenuSliteActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import moviles.guiass.ui.R;
 
 public class MenuSliteActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +81,19 @@ public class MenuSliteActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_user) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_remove) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_add) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_login) {
+
+        } else if (id == R.id.nav_signoff) {
 
         }
 
