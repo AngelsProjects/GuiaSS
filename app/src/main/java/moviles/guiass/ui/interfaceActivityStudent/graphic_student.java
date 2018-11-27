e922f5f717ea7d1caca1b58a11a20881b95229fb
AGUSTINvidal <42757199+AGUSTINvidal@users.noreply.github.com>
2018-11-27 11:00:49 -0800

    Add Activity Student

---
+++ app/src/main/java/moviles/guiass/ui/interfaceActivityStudent/graphic_student.java
@@ -0,0 +1,13 @@
+package moviles.guiass.ui.interfaceActivityStudent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import moviles.guiass.ui.R;
+public class graphic_student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic_student);
+    }
+}
