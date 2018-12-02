f3579ddb725c00310cdfc5d709971244c5ce43bd
AGUSTINvidal <42757199+AGUSTINvidal@users.noreply.github.com>
2018-12-02 00:08:17 -0800

    Modify Activits "Docs servicio social"

--- app/src/main/java/moviles/guiass/ui/Moresteps/ReportActivity.java
+++ app/src/main/java/moviles/guiass/ui/Moresteps/ReportActivity.java
@@ -6,13 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import moviles.guiass.ui.R;
 
+import static moviles.guiass.ui.R.id.imageView_peri;
+
 public class ReportActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.btnFinal)
     Button btnViewMore;
+    @BindView(R.id.imageView_winrar)
+    ImageView imageView_winrar;
+    @BindView(R.id.imageView_peri)
+    ImageView imageView_peri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +32,20 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
         btnViewMore.setOnClickListener(this);
-
+        Picasso.get()
+                .load("http://lechtchenko.azurewebsites.net/img/repdoc.png")
+                // .placeholder(R.drawable.plandoc)   // optional
+                // .error(R.drawable.plandoc)      // optional
+                .resize(400, 400)
+                .into(imageView_winrar);
+        Picasso.get()
+                .load("http://lechtchenko.azurewebsites.net/img/termdoc.png")
+                // .placeholder(R.drawable.plandoc)   // optional
+                // .error(R.drawable.plandoc)      // optional
+                .resize(400, 400)
+                .into(imageView_peri);
     }
+
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/moviles/guiass/ui/serviceDocuments/SolicitudServicio.java
+++ app/src/main/java/moviles/guiass/ui/serviceDocuments/SolicitudServicio.java
@@ -2,13 +2,38 @@ package moviles.guiass.ui.serviceDocuments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import moviles.guiass.ui.R;
 
 public class SolicitudServicio extends AppCompatActivity {
+    @BindView(R.id.btnViewMore)
+    Button btnViewMore;
+    @BindView(R.id.imageView_ss)
+    ImageView imageView_ss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitud_servicio);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        btnViewMore.setOnClickListener((View.OnClickListener) this);
+        //Loading Image from docs
+        Picasso.get()
+                .load("http://lechtchenko.azurewebsites.net/img/solicitud_servicio_social.png")
+                // .placeholder(R.drawable.plandoc)   // optional
+                // .error(R.drawable.plandoc)      // optional
+                .resize(400, 400)
+                .into(imageView_ss);
     }
 }
+
