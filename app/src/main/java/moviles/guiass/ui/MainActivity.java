360534c06382c141ae60256e33435f1efc81ab8f
Angel Arciniega <mangel.arciniega@gmail.com>
2018-12-04 15:31:19 -0800

    Set Login as main layout & add drawe menu in main layout

--- app/src/main/java/moviles/guiass/ui/MainActivity.java
+++ app/src/main/java/moviles/guiass/ui/MainActivity.java
@@ -1,12 +1,16 @@
 package moviles.guiass.ui;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,76 +19,55 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
-import moviles.guiass.ui.R;
-import moviles.guiass.ui.lateralmenu.MenuSliteActivity;
 
 import static moviles.guiass.Enum.MenuOptionsEnum.*;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-    private NavigationView navigationView;
-    // se hacen binding por medio de butterknife a los objetos de la vista XML en este caso a los TEXTVIEW'S
-    // para ello se les asignan mediante los ID's txt_infoTitle, txt_infoDesc que ya se debieron haber asignado en el XML
-    @BindViews({R.id.btn_req, R.id.btn_dh, R.id.btn_dss, R.id.btn_pp, R.id.btn_ps, R.id.btn_menu})
-    List<Button> btnsInfo;
+public class MainActivity extends AppCompatActivity implements /*View.OnClickListener, */NavigationView.OnNavigationItemSelectedListener {
 
 
-    // El metodo onCreate se inicializa para crear todos los objetos o darles algun valor
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
+   /* @BindViews({R.id.btn_req, R.id.btn_dh, R.id.btn_dss, R.id.btn_pp, R.id.btn_ps})
+    List<Button> btnsInfo;*/
+
+    @BindView(R.id.main_toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // se manda a llamar la funcioin super que en definicion es como un constructor que guarda la instancia del estado actual
         super.onCreate(savedInstanceState);
-
-        // se asigna la vista al Activity en este caso estamos en el activity InformationActivity
-        // y se  le esta asignando la vista del siguiente repositorio
-        // R.layout.activity_information
-        // que en definicion por carpetas: res -> layout -> activity_main.xml
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //damos por hecho que haremos binding a los objetos ya declarados anteriormente para este Activity
         ButterKnife.bind(this);
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        // realizamos un for para asignarle a cada boton un onClickListener
-        // ya asinados por la lista que se realizo antes en butterknife
-        // y etse nos servira para escuchar el clicka de cada boton
-        // se le envia como parametro una vista en etse caso usamos la palabra reservada this
-        // que significa esto o esta clase en la que te encuentras actualmente ya que esta clase es
-        //una vista se puede enviar sin problema alguna
-        for (Button btn : btnsInfo) {
+        setSupportActionBar(toolbar);
+     /*   for (Button btn : btnsInfo) {
             btn.setOnClickListener(this);
-        }
-
-
+        }*/
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    // aqui se encuentra el listener que se les asigno a los botones anteriormente
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }/*
     @Override
     public void onClick(View v) {
-        // Con la clase o metodo Intent podremos mandar a llamar a otro activity
-        // para ello se crea un intent en el cual recive dos parametros
-        // 1: el contexto del paquete de donde se enviara en este caso es desde la clase MainActivity.this el cual es la clase del activity
-        // 2: la clase que del activity que quieres mandar a llamar en etse claso InformationActivity.class para acceder a su clase
         Intent intent;
-
-        // creamos una variable bundle para almacenar las variables que le queremos enviar al activity a invocar
         Bundle bundle = new Bundle();
-
-        // ya que estamos dentro del metodo onclick realizamos una comparacion para ver desde
-        // que boton estamos presionando
         switch (v.getId()) {
 
-            //en cualquiera de los casos agrega al buldle un parametro con su respectivo valor
-            // hay distintos modos de enviar parametros en este caso sera una variable entera por ello
-            // se usa el metodo putInt... en cado caso de ser un string seria putString e igual para
-            // cualquier otro tipo de dato
-            // para ello se hace lo siguiente bundle.putInt("NOMBRE_DEL_PARAMETRO_A_ENVIAR", VALOR_DEL_PARAMETRO);
-            case R.id.btn_menu:
-                intent = new Intent(MainActivity.this, MenuSliteActivity.class);
-                bundle.putInt("InfoOption", RequisitosPrevios.getValue());
-                break;
             case R.id.btn_req:
                 intent = new Intent(MainActivity.this, InformationActivity.class);
                 bundle.putInt("InfoOption", RequisitosPrevios.getValue());
@@ -108,13 +91,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(MainActivity.this, InformationActivity.class);
                 break;
         }
-        // Con nuestra variable intent accedemos a su metodo putExtras para asignarle eseos parametros
-        //cpon variables que ya se les asigno anteriormente
         intent.putExtras(bundle);
 
-        // se inicializa el nuevo activity
         startActivity(intent);
     }
 
+*/
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_remove) {
+
+        } else if (id == R.id.nav_add) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_login) {
+
+        } else if (id == R.id.nav_signoff) {
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
--- app/src/main/java/moviles/guiass/ui/lateralmenu/MenuSliteActivity.java
+++ app/src/main/java/moviles/guiass/ui/lateralmenu/MenuSliteActivity.java
@@ -12,32 +12,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import moviles.guiass.ui.R;
 
 public class MenuSliteActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_slite);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -73,13 +79,7 @@ public class MenuSliteActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-
-   /*     if (id == R.id.nav_user) {
-
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_remove) {
+        if (id == R.id.nav_remove) {
 
         } else if (id == R.id.nav_add) {
 
@@ -91,9 +91,8 @@ public class MenuSliteActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_signoff) {
 
-        }*/
+        }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
