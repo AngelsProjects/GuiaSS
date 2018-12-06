772edd1f32d1bc56c99e61e21de3c8c5ea244587
Angel Arciniega <mangel.arciniega@gmail.com>
2018-12-06 13:37:32 -0800

    Update TeacherInterface.java

--- app/src/main/java/moviles/guiass/ui/interfaceActivity/TeacherInterface.java
+++ app/src/main/java/moviles/guiass/ui/interfaceActivity/TeacherInterface.java
@@ -11,6 +11,8 @@ import moviles.guiass.ui.R;
 
 public class TeacherInterface extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,6 @@ public class TeacherInterface extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
